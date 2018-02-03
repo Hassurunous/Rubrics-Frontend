b8bde0855c13ae04701b57b77c09fef13a8e6cc7
Jeffchiucp <jeffchiu2000@gmail.com>
2018-02-03 07:19:39 -0800

    added Login Component

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>
+        Show Rubric
+      </div>
+    );
+  }
+}
+
+export default Login;
